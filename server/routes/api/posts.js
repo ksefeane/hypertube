078ce3777f5195cd5550ccd0fb5e45877bc78b5b
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-08-11 17:34:08 +0200

    basic database initialization

---
+++ server/routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+//get
+router.get('/', (req, res) => {
+    res.send('hello')
+})
+//add
+
+//delete
+
+module.exports = router

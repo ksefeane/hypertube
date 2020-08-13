5588bba78e46654930dc301ed68275d486d14b91
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-08-13 21:20:07 +0200

    es6

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,19 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const Q = require('../../models/query')
-const userController = require('../../controllers/user')
+import { Router } from 'express'
+const router = Router()
+import { listUsers, registerUser } from '../../controllers/userController'
 
+export default router
 //get
-router.get('/', (req, res) => {
-    Q.fetchall('users', (err, data) => {
-        if (data)
-            res.send(data)
-    })
-})
-
+.get('/', listUsers)
 //add
-router.post('/', userController.registerUser)
-
+.post('/', registerUser)
 //delete
-
-module.exports = router

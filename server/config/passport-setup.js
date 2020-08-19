15fd7b413f8aac2bfe50c14ea5c5c66eadaaa79e
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-08-19 10:38:03 +0200

    passport auth fetches profile info

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -3,16 +3,29 @@ import Oauth2Strategy from 'passport-oauth2'
 import { keys } from './keys'
 import { User } from '../models/userModel'
 
-export default passport.use(
-    new Oauth2Strategy ({
+let client = new Oauth2Strategy ({
         authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
         tokenURL: 'https://api.intra.42.fr/oauth/token',
         clientID: keys.wtc.clientID,
         clientSecret: keys.wtc.clientSecret,
         callbackURL: '/api/users/auth/42/redirect',
-    }, (token, refreshToken, profile, cb) => {
-        console.log(token)
-        console.log(profile)
-        cb(null, profile)
-    })
-)
+        scope: 'profile'
+    }, (token, refreshToken, params, profile, callback) => {
+        callback(null, profile)
+})
+
+client.userProfile = function (accesstoken, done) {
+    // choose your own adventure, or use the Strategy's oauth client
+    this._oauth2._request("GET", "https://api.intra.42.fr/v2/me", null, null, accesstoken, (err, data) => {
+      if (err) { return done(err); }
+      try {
+          data = JSON.parse( data );
+      }
+      catch(e) {
+        return done(e);
+      }
+      done(null, data);
+    });
+  };
+
+export default passport.use(client)

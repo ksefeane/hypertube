e244a151cfdef8f33384a586b55b148dfb97a9d0
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-08-21 21:17:04 +0200

    stable 42oauth & githuboauth

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,16 +1,16 @@
 import { Router } from 'express'
 const router = Router()
-import { listUsers, registerUser, loginUser, oauthLogin, logoutUser } from '../../controllers/userController'
+import { listUsers, registerUser, loginUser, ftLogin, gitLogin, logoutUser } from '../../controllers/userController'
 import passport from 'passport'
 
 export default router
 
 //get
 .get('/', listUsers)
-.get('/auth/42', passport.authenticate('oauth2', {scope: 'public'}))
-.get('/auth/github', passport.authenticate('oauth2', {scope: 'user'}))
-.get('/auth/redirect', passport.authenticate('oauth2', 
-    {session: false, failureRedirect: '/api/users/signup'}), oauthLogin)
+.get('/auth/42', passport.authenticate('42', {scope: 'public'}))
+.get('/auth/redirect', passport.authenticate('42', {session: false, failureRedirect: '/api/users/signup'}), ftLogin)
+.get('/auth/github', passport.authenticate('github', {scope: 'user'}))
+.get('/auth/redirect2', passport.authenticate('github', {session: false, failureRedirect: '/api/users/signup'}), ftLogin)
 .get('/logout', logoutUser)
 
 //add

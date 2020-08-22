202f72f089376418c1a959fb445efaa52ad98dc5
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-08-22 12:28:55 +0200

    edited main

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-//thabang's adding a comment
-//kori's comment
-//ora's comment
-//Enoch's comment
+//thabang's adding a comment *
+//kori's comment *
+//ora's comment *
+//Enoch's comment *
 createApp(App).mount('#app')

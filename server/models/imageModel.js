bd2068d59b2f4fa868b8e58f7e3e32eee542b546
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-08-31 10:52:33 +0200

    profile upload beta

--- server/models/imageModel.js
+++ server/models/imageModel.js
@@ -7,13 +7,12 @@ const storage = multer.diskStorage({
 		const tempPath = path.join(__dirname, '../public/uploads')
 		if (!fs.existsSync(tempPath)) {
 			fs.mkdirSync(tempPath)
-			fs.mkdirSync(path.join(tempPath, 'temp'))
+			fs.mkdirSync(path.join(tempPath, 'profile'))
 		}
-		else if (!fs.existsSync(path.join(tempPath, 'temp'))) {
-			fs.mkdirSync(path.join(tempPath, 'temp'))
+		else if (!fs.existsSync(path.join(tempPath, 'profile'))) {
+			fs.mkdirSync(path.join(tempPath, 'profile'))
 		}
-		var dest = 'uploads/temp'
-		callback(null, tempPath+'/temp')
+		callback(null, tempPath+'/profile')
 	},
 	filename: (req, file, callback) => {
 		var save = `${req.user.username}`

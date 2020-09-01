7cbcfa128e7e0e5c40109c71a6d631f34e9d27c2
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-09-01 08:32:18 +0200

    Revert "unfinished torrent downloading setup"
    This reverts commit 3712e3d7684e17cbec7c5d82b53ba1a4b2e2e2a2.

--- server/controllers/videoController.js
+++
@@ -1,6 +0,0 @@
-import { downloadTorrent } from '../models/torrent'
-
-export function downloadVideo(req, res) {
-    downloadTorrent(req.body.url)
-    res.send('test')
-}
--- server/routes/api/video.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { downloadVideo } from '../../controllers/videoController'
-const router = Router()
-
-export default router
-
-.post('/download', downloadVideo)
-
-//download torrent

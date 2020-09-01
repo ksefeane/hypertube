95b9180a137a86f1ff6caa87887ecd6a5cb67fca
ksefeane <ksefeane@student.wethinkcode.co.za>
2020-09-01 17:12:39 +0200

    stream test

--- client/src/views/Video.vue
+++ client/src/views/Video.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container">
         <div class="c-video">
-            <video controls width="100%">
+            <video controls>
+                <source src="http://localhost:5000/api/video/stream" type="video/mp4"> 
                 sorry, doesn't support
             </video>
         </div>
--- server/routes/api/video.js
+++ server/routes/api/video.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { downloadVideo } from '../../controllers/videoController'
+import { downloadVideo, streamVideo } from '../../controllers/videoController'
 const router = Router()
 
 export default router
 
+.get('/stream', streamVideo)
 .post('/download', downloadVideo)
 
 //download torrent

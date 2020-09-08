eec04744abb0764befe6a091c000f137557fb2ef
omputle <omputle@student.wethinkcode.co.za>
2020-09-08 19:42:54 +0200

    Still needs to download and stream

--- client/src/views/MovieDetails.vue
+++ client/src/views/MovieDetails.vue
@@ -1,32 +1,49 @@
 <template>
     <div>
-        <h1>{{ id }}</h1>
-        <!-- <p>{{ film }}</p> -->
-        <img :src="film.large_cover_image" alt="">
-        <br>
-        <small>Score: {{ film.rating }}</small> <br>
-        <small>Rating: {{ film.mpa_rating }}</small>
-        <br>
-        <p>Year: {{ film.year }}</p>
-        <p>Runtime: {{ film.runtime }} minutes</p>
-        <p>{{ film.summary }}</p>
-        <button @click="download_film">Download {{ id }}</button>
+        <app-header></app-header>
+        <div>
+            <div v-if="show">
+                <video controls src="../clips/clip.mp4">
+                No video support
+                </video>
+            </div>
+            <h1>{{ id }}</h1>
+            <!-- <p>{{ film }}</p> -->
+            <img :src="film.large_cover_image" alt="">
+            <br>
+            <small>Score: {{ film.rating }}</small> <br>
+            <small>Rating: {{ film.mpa_rating }}</small>
+            <br>
+            <p>Year: {{ film.year }}</p>
+            <p>Runtime: {{ film.runtime }} minutes</p>
+            <p>{{ film.summary }}</p>
+            <button @click="download_film" v-if="!show">Download {{ id }}</button>
+        </div>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import EventBus from "../event_bus/event_bus";
 
 export default {
+    components: {
+        'app-header': Header,
+        'app-footer': Footer,
+    },
     data() {
         return {
             film: {},
             id: this.$route.params.id,
+            show: false
         }
     },
     methods: {
         download_film() {
-            this.$router.push('/play')
+            // tell backend to download movie
+            this.show = true
         }
     },
     mounted() {
--- client/src/views/PlayMovie.vue
+++ client/src/views/PlayMovie.vue
@@ -1,26 +1,16 @@
 <template>
     <div>
-        <app-header></app-header>
-        <div v-if="play">
-            <video controls src="../clips/clip.mp4" autoplay >
+        <div>
+            <video controls src="../clips/clip.mp4">
             No video support
             </video>
         </div>
-        <div v-else>
-            <button @click="play_movie">Play {{ movie }}</button>
-        </div>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+
 export default {
-    components: {
-        'app-header': Header,
-        'app-footer': Footer
-    },
     data() {
         return {
             play: false,

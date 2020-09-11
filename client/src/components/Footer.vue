e0e2bee226653303fa066ed314a205656d63ae60
Lebo <mmodisad@student.wethinkcode.co.za>
2020-09-11 22:25:06 +0200

    updated footer component

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="footer">
-      <footer>
-          <h6>{{ title }}</h6>
-      </footer>
+    <footer>
+      <h6>{{ title }}</h6>
+    </footer>
   </div>
 </template>
-
 <script>
 export default {
-    data() {
-        return {
-            title: '© Hypertube 2020'
-        }
-    }
-}
+  data() {
+    return {
+      title: "© Hypertube 2020",
+    };
+  },
+};
 </script>
+
 <style scoped>
 .footer {
   position: fixed;
   left: 0;
   bottom: 0;
   width: 100%;
-  background-color: #f7f7f7;
   color: #0275d8;
   text-align: right;
-  font-size: 12px;
-  /* size: 10px; */
+  background-color: #b5afbc;
 }
-
+.h6 {
+  font-size: 1.5px;
+}
 </style>

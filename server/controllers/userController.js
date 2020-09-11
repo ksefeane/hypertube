e57ffed64a1fca5817451838371abf98f9d4c93e
Lebo <mmodisad@student.wethinkcode.co.za>
2020-09-11 22:40:49 +0200

    including controler

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,35 @@
-import { User, fetchUsers, signupUser, signinUser } from '../models/userModel'
+import { 
+    User, fetchUsers, signupUser, signinUser, uploadImage, sendEmailLink, checkEmailLink, setPassword
+} from '../models/userModel'
+import { verify } from 'jsonwebtoken'
+
+export function auth(req, res, next) {
+    req.isAuthenticated() ? next() : 
+        req.user ? next() : res.send({'error': 'access token denied'})
+}
+
+export function jwtauth(req, res, next) {
+    try {
+        const token = req.headers.authorization.replace("Bearer ", "")
+        console.log(token)
+        const decode = verify(token, "secret")
+        req.user = decode
+        next()
+    } catch (e) {
+        return res.status(401).json({
+            message: "authentication failed"
+        })
+    }
+}
 
 export async function listUsers(req, res) {
     var f = await fetchUsers()
-    res.send(f)
+    res.send(req.user)
 }
 
 export async function registerUser(req, res, next) {
     var user = new User(req.body)
+    req.user = user.username
     var stat = await signupUser(user)
     res.send(stat)
 }
@@ -14,22 +37,41 @@ export async function registerUser(req, res, next) {
 export async function loginUser(req, res, next) {
     var user = new User(req.body)
     var stat = await signinUser(user)
-    res.send(stat)
+    res.status(201).json(stat)
 }
 
-export function ftLogin(req, res, next) {
-    //handle with passport
-    
-    res.redirect('/api/users')
-}
-
-export function gitLogin(req, res) {
-    console.log(req.user)
-    req.user = null
+export function authLogin(req, res, next) {
     res.redirect('/api/users')
 }
 
 export function logoutUser(req, res) {
-    //handle with passport
-    res.send('logout')
+    req.logout()
+    res.redirect('/api/users/auth/42')
+}
+export async function passwordReset(req, res) {
+    var username = req.body.username
+    var stat = await sendEmailLink(username).catch(e => {console.log(e)})
+    res.send(stat)
+}
+
+export async function checkLink(req, res, next) {
+    var stat = await checkEmailLink(req.params.reset)
+    stat ? next() : res.send({'error': 'invalid token'})
+}
+
+export async function changePassword(req, res) {
+    var stat = req.body.password.length > 0 ? 
+    await setPassword(req.params.reset, req.body.password) : {'success': 'token valid, please enter password'}
+    res.send(stat)
+}
+
+export async function uploadPhoto(req, res) {
+    if (!req.file) {
+        res.send('error please upload a valid picture')
+        var stat = await uploadImage(req.user)
+    } else {
+        console.log('image uploaded')
+        var stat = await uploadImage(req.user)
+        res.send(stat)
+    }
 }

b4d308d37703ebe2182cc97a2cb599d3caea0c79
Lebo <mmodisad@student.wethinkcode.co.za>
2020-09-11 22:30:07 +0200

    updated Home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
+    <div class="bg"></div>
     {{ msg }}
-    <router-link to="/register">Register</router-link> | 
-    <router-link to="/login">Log in</router-link> 
+    <router-link to="/register">Register</router-link>|
+    <router-link to="/login">Log in</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
 
 export default {
   // name: 'Home',
@@ -17,8 +17,24 @@ export default {
   // }
   data() {
     return {
-      msg: 'Welcome to Hypertube!!'
-    }
-  }
-}
+      msg: "Welcome to Hypertube!!",
+    };
+  },
+};
 </script>
+<style lang="scss" scoped>
+/* @media only screen and (max-width: 400px) { */
+.bg {
+  
+  /* The image used */
+  background-image: url("https://nhsportpress.com/wp-content/uploads/2018/12/movie-collage.png");
+
+  /* Half height */
+  height: 500px;
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+/* } */
+</style>

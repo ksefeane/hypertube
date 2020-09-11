3b3b1655d000ab7e657c4003f5180376f3c94f02
omputle <omputle@student.wethinkcode.co.za>
2020-09-11 23:26:06 +0200

    Can update details

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -3,9 +3,16 @@
         <app-header></app-header>
         <div>
             <h1>Hello {{ uid }}</h1>
+            <div v-for="update in updates" :key="update">
+                <small>{{ update }}</small>
+            </div>
             <!-- <canvas id="profile_pic"></canvas> -->
             <input type="file" name="" id="" @change="onFileSelected"> <br>
             <button @click="uploadImage">Upload</button>
+            <form>
+                <input type="text" v-model="username"> <br>
+            </form>
+            <input type="submit" value="Update Username" @click="update_username">
             <br>
             <form>
                 <input type="text" v-model="first_name"> <br>
@@ -15,10 +22,6 @@
                 <input type="text" v-model="last_name"> <br>
             </form>
             <input type="submit" value="Update Last Name" @click="update_last">
-            <form>
-                <input type="text" v-model="username"> <br>
-            </form>
-            <input type="submit" value="Update Username" @click="update_username">
             <form>
                 <input type="email" v-model="email"> <br>
             </form>
@@ -47,11 +50,12 @@ export default {
             id: this.$route.params.id,
             uid: localStorage.getItem('user'),
             username: '',
-            first_name: 'Marcellus',
-            email: 'marcel@wool.com',
-            last_name: 'Wallace',
+            first_name: '',
+            email: '',
+            last_name: '',
             message: 'this is the profile page',
-            selectedFile: null
+            selectedFile: null,
+            updates: []
         }
     },
     // computed: {
@@ -66,7 +70,7 @@ export default {
         },
         uploadImage() {
             console.log(this.selectedFile);
-            console.log(this.id)
+            // console.log(this.id)
             let type = this.selectedFile.type
             // const url = ''
             // console.log('name: ' + this.selectedFile.name);
@@ -76,13 +80,12 @@ export default {
             const path = 'http://localhost:5000/api/users/upload/' + this.id
             let nn = type.split('/')
             if (nn[0] === 'image') {
-                console.log("We've got an IMAGE!")
                 axios.post(path, {
                     'image': this.selectedFile
                 }).then((result) => {
                     console.log(result)
                 }).catch((error) => {
-                    console.log(error)
+                    console.log("yohohoho", error)
                 })
             } else if (nn[0] === 'video') {
                 console.log("We've got a VIDEO!");
@@ -92,30 +95,72 @@ export default {
             //     'image': this.selectedFile
             // })
         },
-        update_details(field, value) {
-            // const path = 'http://localhost:5000/api/users/signup/'
-            // axios.post(path, {
-            //     field: value
-            // }).then((response) => {
-            //     console.log(response)
-            // }).catch((err) => {
-            //     console.log(err)
-            // })
-            alert(field + ' : ' + value)
+        update_details(data, url) {
+            const path = 'http://localhost:5000/api/users/update-' + url
+            axios.post(path, data).then((response) => {
+                console.log(response)
+            }).catch((err) => {
+                console.log(err)
+            })
         },
         update_username() {
-            this.update_details('username', this.username)
+            this.updates = []
+            let data = {
+                'email': this.email, 
+                'username': this.username
+            }
+            this.update_details(data, 'username/')
+            this.updates.push("Username updated")
             localStorage.setItem('user', this.username)
         },
         update_first() {
-            this.update_details('first_name', this.first_name)
+            this.updates = []
+            let data = {
+                'email': this.email, 
+                'first_name': this.first_name
+            }
+            this.update_details(data, 'first/')
+            this.updates.push("First name updated")
         },
         update_last() {
-            this.update_details('last_name', this.last_name)
+            this.updates = []
+            let data = {
+                'email': this.email, 
+                'last_name': this.last_name
+            }
+            this.update_details(data, 'last/')
+            this.updates.push("Last name updated")
         },
         update_email() {
-            this.update_details('email', this.email)
+            this.updates = []
+            let data = {
+                'email': this.email, 
+                'username': this.username
+            }
+            this.update_details(data, 'email/')
+            this.updates.push("Email updated")
         },
+        fetch_user_data() {
+            // fetch user info and paste it on the form for editing
+            const path = 'http://localhost:5000/api/users/update/'
+            axios.post(path, {
+                'username': this.id
+            }).then((result) => {
+                console.log('It worked')
+                console.log(result.data[0])
+                if (result.data[0]) {
+                    this.email = result.data[0].email
+                    this.last_name = result.data[0].last_name
+                    this.first_name = result.data[0].first_name
+                    this.username = result.data[0].username
+                }
+            }).catch((err) => {
+                console.log(err)
+            })
+        }
+    },
+    mounted() {
+        this.fetch_user_data()
     }
 }
 </script>
